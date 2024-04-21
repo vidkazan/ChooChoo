e3859c003020585ad673e1b6dff7ca580156ad26
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-04-21 23:41:46 +0200

    cllocation: bearingTo location

--- ChooChoo/Helpers/Location/CLLOcationCoordinate2D+.swift
+++ ChooChoo/Helpers/Location/CLLOcationCoordinate2D+.swift
@@ -31,3 +31,41 @@ extension CLLocation {
 		return self.distance(from: CLLocation(latitude: from.latitude, longitude: from.longitude))
 	}
 }
+
+extension CLLocation {
+	func getRadiansFrom(degrees: Double ) -> Double {
+		return degrees * .pi / 180
+	}
+
+	func getDegreesFrom(radians: Double) -> Double {
+		return radians * 180 / .pi
+	}
+
+
+	func bearingRadianTo(location: CLLocation) -> Double {
+
+		let lat1 = self.getRadiansFrom(degrees: self.coordinate.latitude)
+		let lon1 = self.getRadiansFrom(degrees: self.coordinate.longitude)
+
+		let lat2 = self.getRadiansFrom(degrees: location.coordinate.latitude)
+		let lon2 = self.getRadiansFrom(degrees: location.coordinate.longitude)
+
+		let dLon = lon2 - lon1
+
+		let y = sin(dLon) * cos(lat2)
+		let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
+
+		var radiansBearing = atan2(y, x)
+
+		if radiansBearing < 0.0 {
+			radiansBearing += 2 * .pi
+		}
+
+
+		return radiansBearing
+	}
+
+	func bearingDegreesTo(location: CLLocation) -> Double {
+		return self.getDegreesFrom(radians: self.bearingRadianTo(location: location))
+	}
+}
