6390199334ba529640298de6bca1d52dfcd69a66
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-04-21 14:44:47 +0200

    - NearestStopVIewModel

---
+++ ChooChoo/Feature/JourneySearch/NearestStop/vm/NearestStopViewModel.swift
@@ -0,0 +1,394 @@
+//
+//  NearestStopViewModel.swift
+//  ChooChoo
+//
+//  Created by Dmitrii Grigorev on 19.04.24.
+//
+
+import Foundation
+import Combine
+import CoreLocation
+import MapKit
+import SwiftUI
+
+class NearestStopViewModel : ChewViewModelProtocol {
+	@Published private(set) var state : State {
+		didSet { Self.log(state.status) }
+	}
+	private var bag = Set<AnyCancellable>()
+	private let input = PassthroughSubject<Event,Never>()
+	
+	
+	init(_ initaialStatus : Status) {
+		self.state = State(
+			data: StateData(
+				stops: [],
+				selectedStop: nil
+			),
+			status: initaialStatus
+		)
+		Publishers.system(
+			initial: state,
+			reduce: Self.reduce,
+			scheduler: RunLoop.main,
+			feedbacks: [
+				Self.userInput(input: input.eraseToAnyPublisher()),
+				Self.whenLoadingNearbyStops(),
+				Self.whenLoadingStopDetails()
+			]
+		)
+		.weakAssign(to: \.state, on: self)
+		.store(in: &bag)
+	}
+	
+	deinit {
+		bag.removeAll()
+	}
+
+	func send(event: Event) {
+		input.send(event)
+	}
+}
+
+extension NearestStopViewModel {
+	struct State {
+		let data : StateData
+		let status : Status
+	}
+	
+	struct StateData {
+		let stops : [StopWithDistance]
+		let selectedStop : StopWithDistance?
+		let selectedStopTrips : [LegViewData]?
+		
+		init(stops: [StopWithDistance], selectedStop: StopWithDistance?) {
+			self.stops = stops
+			self.selectedStop = selectedStop
+			self.selectedStopTrips = nil
+		}
+		init(stops: [StopWithDistance], selectedStop: StopWithDistance?,trips : [LegViewData]?) {
+			self.stops = stops
+			self.selectedStop = selectedStop
+			self.selectedStopTrips = trips
+		}
+	}
+	
+	enum Status :  ChewStatus {
+		case idle
+		case error(any ChewError)
+		case submitting(StopWithDistance)
+		case loadingStopDetails(
+			StopWithDistance,_ send : (MapPickerViewModel.Event)->Void
+		)
+		case loadingNearbyStops(_ region : MKCoordinateRegion)
+		var description : String {
+			switch self {
+			case .error(let err):
+				return "error \(err.localizedDescription)"
+			case .submitting:
+				return "submitting"
+			case .idle:
+				return "idle"
+			case .loadingStopDetails:
+				return "loadingStopDetails"
+			case .loadingNearbyStops:
+				return "loadingNearbyStops"
+			}
+		}
+	}
+	
+	enum Event : ChewEvent {
+		case didDeselectStop
+		case didSubmitStop(StopWithDistance)
+		case didTapStopOnMap(StopWithDistance,send : (MapPickerViewModel.Event)->Void)
+		case didDragMap(_ region : MKCoordinateRegion)
+		
+		case didLoadStopDetails(StopWithDistance,_ stopTrips : [LegViewData])
+		
+		case didLoadNearbyStops([StopWithDistance])
+		
+		case didCancelLoading
+		case didFailToLoad(any ChewError)
+		var description : String {
+			switch self {
+			case .didDeselectStop:
+				return "didDeselectStop"
+			case .didFailToLoad(let err):
+				return "didFailToLoad \(err.localizedDescription)"
+			case .didCancelLoading:
+				return "didCancelLoading"
+			case .didSubmitStop(let stop):
+				return "didSelectStop \(stop.stop.name)"
+			case .didTapStopOnMap(let stop,_):
+				return "didTapStopOnMaps \(stop.stop.name)"
+			case .didDragMap:
+				return "didDragMap"
+			case .didLoadStopDetails(_,_):
+				return "didLoadStopDetails"
+			case .didLoadNearbyStops(_):
+				return "didLoadNearbyStops"
+			}
+		}
+	}
+}
+
+
+extension NearestStopViewModel {
+	static func reduce(_ state: State, _ event: Event) -> State {
+		Self.log(event, state.status)
+		switch state.status {
+		case .idle,.error:
+			switch event {
+			case .didFailToLoad(let err):
+				return State(data: state.data, status: .error(err))
+			case .didCancelLoading:
+				return state
+			case .didDeselectStop:
+				return State(
+					data: .init(
+						stops: state.data.stops,
+						selectedStop: nil
+					),
+					status: .idle
+				)
+			case .didSubmitStop(let stop):
+				return State(
+					data: StateData(
+						stops: [],
+						selectedStop: nil
+					),
+					status: .submitting(stop)
+				)
+			case let .didTapStopOnMap(stop,send):
+				return State(
+					data: StateData(
+						stops: state.data.stops,
+						selectedStop: stop
+					),
+					status: .loadingStopDetails(stop,send)
+				)
+			case .didDragMap(let coords):
+				return State(
+					data: state.data,
+					status: .loadingNearbyStops(coords)
+				)
+			case let .didLoadStopDetails(stop, trips):
+				return State(
+					data: StateData(
+						stops: state.data.stops,
+						selectedStop: stop.stop.coordinates == state.data.selectedStop?.stop.coordinates ? stop : state.data.selectedStop,
+						trips: trips
+					),
+					status: .idle
+				)
+			case .didLoadNearbyStops(let stops):
+				return State(
+					data: StateData(
+						stops: stops,
+						selectedStop: state.data.selectedStop,
+						trips: state.data.selectedStopTrips
+					),
+					status: .idle
+				)
+			}
+		case .loadingStopDetails:
+			switch event {
+			case .didDeselectStop:
+				return State(
+					data: .init(
+						stops: state.data.stops,
+						selectedStop: nil
+					),
+					status: .idle
+				)
+			case .didFailToLoad(let err):
+				return State(data: state.data, status: .error(err))
+			case .didCancelLoading:
+				return State(data: state.data, status: .idle)
+			case .didSubmitStop(let stop):
+				return State(
+					data: StateData(
+						stops: [],
+						selectedStop: nil
+					),
+					status: .submitting(stop)
+				)
+			case let .didTapStopOnMap(stop,send):
+				return State(
+					data: StateData(
+						stops: state.data.stops,
+						selectedStop: stop
+					),
+					status: .loadingStopDetails(stop,send)
+				)
+			case .didDragMap(let coords):
+				return State(
+					data: state.data,
+					status: .loadingNearbyStops(coords)
+				)
+			case let .didLoadStopDetails(stop, trips):
+				return State(
+					data: StateData(
+						stops: state.data.stops,
+						selectedStop: stop.stop.coordinates == state.data.selectedStop?.stop.coordinates ? stop : state.data.selectedStop,
+						trips: trips
+					),
+					status: .idle
+				)
+			case .didLoadNearbyStops(let stops):
+				return State(
+					data: StateData(
+						stops: stops,
+						selectedStop: state.data.selectedStop,
+						trips: state.data.selectedStopTrips
+					),
+					status: .idle
+				)
+			}
+		case .loadingNearbyStops:
+			switch event {
+			case .didDeselectStop:
+				return State(
+					data: .init(
+						stops: state.data.stops,
+						selectedStop: nil
+					),
+					status: .idle
+				)
+			case .didFailToLoad(let err):
+				return State(data: state.data, status: .error(err))
+			case .didCancelLoading:
+				return State(data: state.data, status: .idle)
+			case .didSubmitStop(let stop):
+				return State(
+					data: StateData(
+						stops: [],
+						selectedStop: nil
+					),
+					status: .submitting(stop)
+				)
+			case let .didTapStopOnMap(stop,send):
+				return State(
+					data: StateData(
+						stops: state.data.stops,
+						selectedStop: stop
+					),
+					status: .loadingStopDetails(stop,send)
+				)
+			case .didDragMap(let coords):
+				return State(
+					data: state.data,
+					status: .loadingNearbyStops(coords)
+				)
+			case let .didLoadStopDetails(stop, trips):
+				return State(
+					data: StateData(
+						stops: state.data.stops,
+						selectedStop: stop.stop.coordinates == state.data.selectedStop?.stop.coordinates ? stop : state.data.selectedStop,
+						trips: trips
+					),
+					status: .idle
+				)
+			case .didLoadNearbyStops(let stops):
+				return State(
+					data: StateData(
+						stops: stops,
+						selectedStop: state.data.selectedStop,
+						trips: state.data.selectedStopTrips
+					),
+					status: .idle
+				)
+			}
+		case .submitting:
+			return state
+		}
+	}
+}
+
+extension NearestStopViewModel {
+	static func userInput(input: AnyPublisher<Event, Never>) -> Feedback<State, Event> {
+		Feedback { _ in
+			return input
+		}
+	}
+	
+	static func whenLoadingNearbyStops() -> Feedback<State, Event> {
+		Feedback { (state: State) -> AnyPublisher<Event, Never> in
+			guard case .loadingNearbyStops(let region) = state.status else {
+				return Empty().eraseToAnyPublisher()
+			}
+			if region.span.longitudeDelta > 0.02 {
+				return Just(Event.didCancelLoading).eraseToAnyPublisher()
+			}
+			return Self.fetchLocatonsNearby(coords: region.center)
+				.mapError{ $0 }
+				.asyncFlatMap { res in
+					let stops : [StopWithDistance] = res.compactMap{
+						if let stop = $0.stop(),let dist = $0.distance {
+							return StopWithDistance(
+								stop: stop,
+								distance: Double(dist)
+							)
+						} else {
+							return nil
+						}
+					}
+					return Event.didLoadNearbyStops(stops)
+				}
+				.catch { error in
+					return Just(Event.didFailToLoad(error as? ApiError ?? .generic(description: error.localizedDescription))).eraseToAnyPublisher()
+				}
+				.eraseToAnyPublisher()
+		}
+	}
+	
+	static func whenLoadingStopDetails() -> Feedback<State, Event> {
+		Feedback { (state: State) -> AnyPublisher<Event, Never> in
+			guard case let .loadingStopDetails(stop,_) = state.status else {
+				return Empty().eraseToAnyPublisher()
+			}
+			switch stop.stop.type {
+			case .station,.stop:
+				return Self.fetchStopDepartures(stop:stop.stop)
+					.map { tripDTOs in
+						if let departures = tripDTOs.departures {
+							return Event.didLoadStopDetails(stop, departures.compactMap({$0.legViewData(type: .departure)}))
+						}
+						if let arrivals = tripDTOs.arrivals {
+							return Event.didLoadStopDetails(stop, arrivals.compactMap({$0.legViewData(type: .arrival)}))
+						}
+						return Event.didCancelLoading
+					}
+					.catch { err in
+						return Just(Event.didFailToLoad(err)).eraseToAnyPublisher()
+					}
+					.eraseToAnyPublisher()
+			default:
+				return Just(Event.didCancelLoading).eraseToAnyPublisher()
+			}
+		}
+	}
+	
+	static func fetchLocatonsNearby(coords : CLLocationCoordinate2D) -> AnyPublisher<[StopDTO],ApiError> {
+		return ApiService().fetch(
+			[StopDTO].self,
+			query: [
+				Query.longitude(longitude: String(coords.longitude)).queryItem(),
+				Query.latitude(latitude: String(coords.latitude)).queryItem()
+			],
+			type: ApiService.Requests.locationsNearby(coords: coords)
+		)
+		.eraseToAnyPublisher()
+	}
+	
+	static func fetchStopDepartures(stop : Stop) -> AnyPublisher<StopTripsDTO,ApiError> {
+		return ApiService().fetch(
+			StopTripsDTO.self,
+			query: [
+				Query.results(max: 15 ).queryItem()
+			],
+			type: ApiService.Requests.stopDepartures(stopId: stop.id)
+		)
+		.eraseToAnyPublisher()
+	}
+}
