81d2e8953acfe80b9bf31c80f839c7c7f9fd9e60
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-04-25 14:24:17 +0200

    - fix

--- ChooChoo/Helpers/View/TimeLabelView/TimeLabelView.swift
+++ ChooChoo/Helpers/View/TimeLabelView/TimeLabelView.swift
@@ -63,63 +63,6 @@ extension TimeLabelView {
 	}
 }
 
-struct ArrivalTrainTimeLabelView: View {
-	@ObservedObject var arrivingTrainVM : ArrivingTrainTimeViewModel
-	let legViewData : LegViewData
-	init(
-		arrivingTrainVM: ArrivingTrainTimeViewModel = ArrivingTrainTimeViewModel(),
-		baseTimeLabel: TimeLabelView,
-		legViewData : LegViewData
-	) {
-		self.arrivingTrainVM = arrivingTrainVM
-		self.baseTimeLabel = baseTimeLabel
-		self.legViewData = legViewData
-	}
-	
-	let baseTimeLabel : TimeLabelView
-	
-	var body : some View {
-		VStack(spacing: 0) {
-			Button(action: {
-				switch arrivingTrainVM.state.status {
-				case .idle,.error:
-					arrivingTrainVM.send(event: .didRequestTime(leg: legViewData))
-				case .loading:
-					arrivingTrainVM.send(event: .didCancelRequestTime)
-				}
-			}, label: {
-				switch arrivingTrainVM.state.status {
-				case .idle:
-					if let time = arrivingTrainVM.state.time {
-						TimeLabelView(
-							size: .medium,
-							arragement: .bottom,
-							delayStatus: .onTime,
-							time: time
-						)
-						.badgeBackgroundStyle(.primary)
-						.padding(.top,2)
-					}
-				case .loading:
-					ProgressView()
-						.chewTextSize(.medium)
-						.padding(2)
-				case .error:
-					EmptyView()
-				}
-			})
-			.transition(.slide)
-			.onAppear {
-				arrivingTrainVM.send(event: .didRequestTime(leg: legViewData))
-			}
-			baseTimeLabel
-		}
-	}
-	
-}
-
-
-
 #if DEBUG
 struct TimeLabelPreviews: PreviewProvider {
 	static var previews: some View {
