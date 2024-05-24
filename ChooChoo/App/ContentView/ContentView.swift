4013c25b3b33c3de32e525c356d4cf5ebce12b2f
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-05-24 15:17:35 +0200

    - moved all issues to jira

--- ChooChoo/App/ContentView/ContentView.swift
+++ ChooChoo/App/ContentView/ContentView.swift
@@ -8,24 +8,6 @@
 import SwiftUI
 import TipKit
 
-// make colored trasport legView as default
-// API unavailable policy
-// SSV: optimise request rate while searching the stop
-// NSV: after tap on stop add icon to get foot path on map to this stop
-// SSV: search field right button: location is not updating
-// SSV: all widgets depend and reload on every chewVM state change
-// map picker: tip for long tap / fix animation
-// dashboard with speed etc.
-// logging: put logger to choobutton protocol
-// normal privacy policy page
-// normal feedback form
-// used content list
-// mailto
-
-// TODO: feature: ldsv: make stop tappable and show stop details and all leg stopover info
-// TODO: jfv: mapCell: map without interaction, icons
-// performance: main thread is blocked while updating journeys
-// TimeChooserView: long tap on segmented picker -> reload?
 struct ContentView: View {
 	@EnvironmentObject var chewViewModel : ChewViewModel
 	@ObservedObject var alertVM = Model.shared.alertVM
