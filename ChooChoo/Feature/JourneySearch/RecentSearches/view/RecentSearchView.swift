875ece9c959e046f61af96dfd003d3c70e8518eb
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-05-27 12:52:07 +0200

    - file refactor

--- ChooChoo/Feature/JourneySearch/RecentSearches/view/RecentSearchView.swift
+++ ChooChoo/Feature/JourneySearch/RecentSearches/view/RecentSearchView.swift
@@ -9,37 +9,6 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct DepartureArrivalPairStop : Hashable,Codable {
-	let departure : Stop
-	let arrival : Stop
-	let id : String
-	init(departure: Stop, arrival: Stop) {
-		self.departure = departure
-		self.arrival = arrival
-		self.id = departure.name + arrival.name
-	}
-}
-
-struct DepartureArrivalPair<T: Hashable & Codable> : Hashable, Codable {
-	let departure : T
-	let arrival : T
-	init(departure: T, arrival: T) {
-		self.departure = departure
-		self.arrival = arrival
-	}
-}
-
-extension DepartureArrivalPair {
-	func encode() -> Data? {
-		return try? JSONEncoder().encode(self)
-	}
-	static func decode(data: Data) -> Self? {
-		return try? JSONDecoder().decode(Self.self, from: data)
-	}
-}
-
-
-
 struct RecentSearchesView : View {
 	@EnvironmentObject var chewVM : ChewViewModel
 	@ObservedObject var recentSearchesVM : RecentSearchesViewModel = Model.shared.recentSearchesVM
