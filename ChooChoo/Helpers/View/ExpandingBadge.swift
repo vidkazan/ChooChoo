d74d3f267875549a4d58c6b9f8b6c44fb54513e2
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-06-19 15:45:30 +0200

    - added ChooViews module - using ExpandingBadge from module

--- ChooChoo/Helpers/View/ExpandingBadge.swift
+++ ChooChoo/Helpers/View/ExpandingBadge.swift
@@ -7,39 +7,40 @@
 
 import Foundation
 import SwiftUI
+import ChooViews
 
 extension BadgeView {
 	func expandingBadge<V>(@ViewBuilder _ expandedView: @escaping () -> V) -> some View where V : View {
 		ExpandingBadge(
+			BadgeView.self,
 			isExpanded: false,
 			baseBadge: self,
-			expandedView: expandedView
+ 			expandedView: expandedView
 		)
 	}
 }
-
-struct ExpandingBadge<Content: View>: View {
-	@Namespace var ExpandingBadge
-	@State var isExpanded = false
-	let baseBadge : BadgeView
-	let expandedView : () -> Content
-	var body: some View {
-		Button(action: {
-			withAnimation {
-				isExpanded.toggle()
-			}
-		}, label: {
-			HStack {
-				baseBadge
-				if isExpanded {
-					expandedView()
-						.transition(
-							.opacity.combined(with: .move(edge: .leading))
-						)
-				}
-			}
-			.padding(5)
-		})
-		
-	}
-}
+//
+//struct ExpandingBadge<Content: View,T : View>: View {
+//	@Namespace var ExpandingBadge
+//	@State var isExpanded = false
+//	let baseBadge : T
+//	let expandedView : () -> Content
+//	var body: some View {
+//		Button(action: {
+//			withAnimation {
+//				isExpanded.toggle()
+//			}
+//		}, label: {
+//			HStack {
+//				baseBadge
+//				if isExpanded {
+//					expandedView()
+//						.transition(
+//							.opacity.combined(with: .move(edge: .leading))
+//						)
+//				}
+//			}
+//			.padding(5)
+//		})
+//	}
+//}
