ff770919dd939963219ed4fd279a0eff4b324b6e
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-06-21 17:20:30 +0200

    -file refactor

--- ChooChoo/Model/Network/ChooNetworking.swift
+++ ChooChoo/Model/Network/ChooNetworking.swift
@@ -8,11 +8,101 @@
 import Foundation
 import Combine
 import CoreLocation
+import OSLog
+
+protocol ChooClient {
+	func execute<T:Decodable>(_ t : T.Type,request: URLRequest, type : ChooNetworking.Requests) -> AnyPublisher<T,ApiError>
+}
+
+class ApiClient : ChooClient {
+	func execute<T: Decodable>(
+		_ t : T.Type,
+		request : URLRequest,
+		type : ChooNetworking.Requests
+	) -> AnyPublisher<T, ApiError> {
+		return URLSession.shared
+			.dataTaskPublisher(for: request)
+			.tryMap { data, response -> T in
+				guard let response = response as? HTTPURLResponse else {
+					throw ApiError.cannotDecodeRawData
+				}
+				switch response.statusCode {
+				case 400...599:
+					if let data = try? JSONDecoder().decode(HafasErrorDTO.self, from: data) {
+						throw ApiError.hafasError(data)
+					}
+					throw ApiError.badServerResponse(code: response.statusCode)
+				default:
+					break
+				}
+				let value = try JSONDecoder().decode(T.self, from: data)
+				let url = request.url?.path ?? ""
+				Logger.networking.trace("done: \(type.description)  \(url)")
+				return value
+			}
+			.receive(on: DispatchQueue.main)
+			.mapError{ error -> ApiError in
+				let url = request.url?.path ?? ""
+				Logger.networking.error("\(type.description) \(url) \(error)")
+				switch error {
+				case let error as ApiError:
+					return error
+				default:
+					return .generic(description: error.localizedDescription)
+				}
+			}
+			.eraseToAnyPublisher()
+	}
+}
+
+extension ChooNetworking {
+	func fetch<T: Decodable>(
+		_ t : T.Type,
+		query : [URLQueryItem],
+		type : Requests
+	) -> AnyPublisher<T, ApiError> {
+		guard let url = ChooNetworking.generateUrl(
+			query: query,
+			type: type,
+			host: Constants.apiData.urlBase
+		) else {
+			return Future<T,ApiError> {
+				return $0(.failure(.badUrl))
+			}.eraseToAnyPublisher()
+		}
+		
+		let request = type.getRequest(urlEndPoint: url)
+		return self.client.execute(
+			t.self,
+			request: request,
+			type: type
+		)
+	}
+}
+
+class MockClient : ChooClient {
+	
+	var inputRequest: URLRequest?
+	var executeCalled = false
+	var requestType : ChooNetworking.Requests?
+	
+	func execute<T: Decodable>(
+		_ t : T.Type,
+		request : URLRequest,
+		type : ChooNetworking.Requests
+	) -> AnyPublisher<T, ApiError> {
+		executeCalled = true
+		inputRequest = request
+		requestType = type
+		return Empty().eraseToAnyPublisher()
+	}
+}
+
 
 class ChooNetworking  {
-	let client : ChewClient
+	let client : ChooClient
 	
-	init(client : ChewClient = ApiClient()) {
+	init(client : ChooClient = ApiClient()) {
 		self.client = client
 	}
 }
