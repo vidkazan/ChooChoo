d574ba5a5435705835f0f5cdfa476c2065ef8f94
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-07-14 19:34:54 +0200

    - ReferenceTimeSliderView: added time range settings

--- ChooChoo/Feature/Debug/ReferenceTimeSliderView.swift
+++ ChooChoo/Feature/Debug/ReferenceTimeSliderView.swift
@@ -8,52 +8,97 @@
 import Foundation
 import SwiftUI
 
+enum TimeSliderRangeCases : Double, Hashable, CaseIterable {
+	case tenMinutes = 600
+	case thirtyMinutes = 1800
+	case oneHour = 3600
+	case threeHours = 10800
+	
+	var description : String {
+		switch self {
+		case .tenMinutes:
+			return "10min"
+		case .thirtyMinutes:
+			return "30min"
+		case .oneHour:
+			return "1h"
+		case .threeHours:
+			return "3h"
+		}
+	}
+}
+
 struct ReferenceTimeSliderView : View {
 	@EnvironmentObject var chewVM : ChewViewModel
 	@State var sliderValue : Double = 0
+	@State var disclosedSliderSettings : Bool = false
  	@State var initialReferenceDate : ChewDate = .now
-	static let timeRangeInSeconds : Double = 1200
+	@State var timeRangeInSeconds : TimeSliderRangeCases = .tenMinutes
 	
 	var body: some View {
-		HStack {
-			Button("now", action: {
-				sliderValue = 0
-			})
-			.padding(5)
-			.chewTextSize(.big)
-			.badgeBackgroundStyle(.accent)
-			Slider(
-				value: $sliderValue,
-				in: -1...1
-			)
-			.padding(5)
-			.onChange(of: sliderValue, perform: { val in
-				withAnimation {
-					chewVM.referenceDate = date(val: val)
-				}
-			})
-			Text(date(val:sliderValue).date, style: .time)
+		VStack {
+			HStack(spacing: 1) {
+				Button("now", action: {
+					sliderValue = 0
+				})
 				.padding(5)
 				.chewTextSize(.big)
-				.badgeBackgroundStyle(.secondary)
-			Button(action: {
-				Model.shared.appSettingsVM.send(event: .didRequestToUpdateAppSettings(
-					settings: AppSettings(
-						debugSettings: .init(
-							prettyJSON: Model.shared.appSettingsVM.state.settings.debugSettings.prettyJSON,
-							alternativeSearchPage: Model.shared.appSettingsVM.state.settings.debugSettings.alternativeSearchPage,
-							timeSlider: false
-						),
-					legViewMode: Model.shared.appSettingsVM.state.settings.legViewMode,
-					tips: Model.shared.appSettingsVM.state.settings.tipsToShow)
-				))
-			}, label: {
-				Image(.xmarkCircle)
+				.badgeBackgroundStyle(.accent)
+				Slider(
+					value: $sliderValue,
+					in: -1...1
+				)
+				.padding(5)
+				.onChange(of: sliderValue, perform: { val in
+					withAnimation {
+						chewVM.referenceDate = date(val: val)
+					}
+				})
+				Text(date(val:sliderValue).date, style: .time)
+					.padding(5)
 					.chewTextSize(.big)
-					.tint(.gray)
-				
-			})
-			.frame(width: 40,height: 40)
+					.badgeBackgroundStyle(.secondary)
+				Button(action: {
+					disclosedSliderSettings.toggle()
+				}, label: {
+					Image(.sliderHorizontal)
+						.chewTextSize(.big)
+						.tint(.gray)
+					
+				})
+				.frame(width: 40,height: 40)
+				Button(action: {
+					Model.shared.appSettingsVM.send(event: .didRequestToUpdateAppSettings(
+						settings: AppSettings(
+							debugSettings: .init(
+								prettyJSON: Model.shared.appSettingsVM.state.settings.debugSettings.prettyJSON,
+								alternativeSearchPage: Model.shared.appSettingsVM.state.settings.debugSettings.alternativeSearchPage,
+								timeSlider: false
+							),
+							legViewMode: Model.shared.appSettingsVM.state.settings.legViewMode,
+							tips: Model.shared.appSettingsVM.state.settings.tipsToShow)
+					))
+				}, label: {
+					Image(.xmarkCircle)
+						.chewTextSize(.big)
+						.tint(.gray)
+					
+				})
+				.frame(width: 40,height: 40)
+			}
+			if disclosedSliderSettings == true {
+				HStack {
+					Picker(selection: $timeRangeInSeconds, content: {
+						ForEach(TimeSliderRangeCases.allCases,id: \.hashValue, content: {
+							Text(verbatim: $0.description)
+								.tag($0)
+						})
+					}, label: {
+						Text(verbatim: "Slider time range")
+					})
+					.pickerStyle(.segmented)
+				}
+			}
 		}
 		.onAppear {
 			self.initialReferenceDate = chewVM.referenceDate
@@ -67,7 +112,7 @@ struct ReferenceTimeSliderView : View {
 
 extension ReferenceTimeSliderView {
 	func date(val : Double) -> ChewDate {
-		return .specificDate(initialReferenceDate.ts + (Self.timeRangeInSeconds * val))
+		return .specificDate(initialReferenceDate.ts + (timeRangeInSeconds.rawValue * val))
 	}
 }
 
