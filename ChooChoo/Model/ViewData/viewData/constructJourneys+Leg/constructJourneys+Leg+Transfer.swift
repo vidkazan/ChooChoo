a14b28072b6ac5c1699d3c14c81e892cea892d7a
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-07-16 11:09:11 +0200

    -  constructTransferViewData: isReachable depends on fromLeg.isReachable

--- ChooChoo/Model/ViewData/viewData/constructJourneys+Leg/constructJourneys+Leg+Transfer.swift
+++ ChooChoo/Model/ViewData/viewData/constructJourneys+Leg/constructJourneys+Leg+Transfer.swift
@@ -34,7 +34,7 @@ func constructTransferViewData(fromLeg : LegDTO, toLeg : LegDTO) -> LegViewData?
 	)
 	
 	let res = LegViewData(
-		isReachable: true,
+		isReachable: fromLeg.reachable ?? true,
 		legType: .transfer,
 		tripId: UUID().uuidString,
 		direction: toLeg.origin?.name ?? "transfer direction",
