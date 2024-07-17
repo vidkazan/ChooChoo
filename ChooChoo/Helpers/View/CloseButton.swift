3f9ed18a1ce24e15b3639832c3b7bdd34a5629b7
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-07-17 14:58:28 +0200

    - closeButton view

--- ChooChoo/Helpers/View/CloseButton.swift
+++ ChooChoo/Helpers/View/CloseButton.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct CloseButton: View {
+	let action : () -> ()
+	
+	init(action : @escaping () -> ()) {
+		self.action = action
+	}
+	
+	var body: some View {
+		Button(action: action, label: {
+			Image(.xmarkCircle)
+				.chewTextSize(.big)
+				.tint(.secondary)
+		})
+	}
+}
