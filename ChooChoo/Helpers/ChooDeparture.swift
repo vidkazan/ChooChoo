db6d4765d6da909cb78c1ccc35553c58c215743a
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-08-01 14:01:52 +0200

    ChooDeparture

---
+++ ChooChoo/Helpers/ChooDeparture.swift
@@ -0,0 +1,31 @@
+//
+//  ChooDeparture.swift
+//  ChooChoo
+//
+//  Created by Dmitrii Grigorev on 01.08.24.
+//
+
+import Foundation
+
+enum ChooDeparture : Equatable,Hashable {
+	case location(Stop)
+	case transport(LegViewData)
+
+	var stop : Stop? {
+		switch self {
+		case .location(let stop):
+			return stop
+		case .transport(let leg):
+			return JourneyAlternativeDepartureStopViewModel.getCurrentLegAlternativeJourneyDepartureStop(leg: leg, referenceDate: .now)?.alternativeDeparture.stopViewData.stop()
+		}
+	}
+	
+	var leg : LegViewData? {
+		switch self {
+		case .location:
+			return nil
+		case .transport(let leg):
+			return leg
+		}
+	}
+}
