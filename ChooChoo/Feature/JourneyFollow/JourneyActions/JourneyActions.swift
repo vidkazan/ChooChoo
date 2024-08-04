0d77602f41b0f1945554e11f54b36625bc238b10
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-08-04 23:23:32 +0200

    JourneyActions struct

--- ChooChoo/Feature/JourneyFollow/JourneyActions/JourneyActions.swift
+++ ChooChoo/Feature/JourneyFollow/JourneyActions/JourneyActions.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+
+extension JourneyFollowData {
+	enum JourneyActionType : Hashable {
+		case enter // | ->
+		case exit // -> |
+		
+		func platform(platform : StopViewData.Platforms) -> Prognosed<String> {
+			switch self {
+			case .enter:
+				return platform.departure
+			case .exit:
+				return platform.arrival
+			}
+		}
+		
+		func time(time : TimeContainer) -> Prognosed<Date> {
+			switch self {
+			case .enter:
+				return time.date.departure
+			case .exit:
+				return time.date.arrival
+			}
+		}
+	}
+	
+	struct JourneyAction : Hashable {
+		let type : JourneyActionType
+		let leg : LegViewData
+		let stopData : StopViewData
+	}
+}
