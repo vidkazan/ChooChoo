a6f5880d22e7ad4cfb0939ca61d5157a8e65da46
Dmitrii Grigorev <vid.kazan@gmail.com>
2024-08-12 15:34:04 +0200

    JSV, JFV: using common toolbar

--- ChooChoo/Feature/JourneyFollow/JourneyFollowView/JFV.swift
+++ ChooChoo/Feature/JourneyFollow/JourneyFollowView/JFV.swift
@@ -69,20 +69,7 @@ struct JourneyFollowView : View {
 			Text("Journey follow", comment: "navigationBarTitle")
 		)
 		.toolbar {
-			ToolbarItem(placement: .topBarLeading, content: {
-				if alertVM.state.alerts.contains(.offline) {
-					Button(action: {
-						UIApplication.shared.open(URL(string: "http://captive.apple.com")!,options: [:], completionHandler: nil)
-					}, label: {
-						BadgeView(.offlineMode)
-							.foregroundColor(.primary)
-							.badgeBackgroundStyle(.blue)
-					})
-				} else if alertVM.state.alerts.contains(.apiUnavailable) {
-					BadgeView(.apiUnavaiable)
-						.badgeBackgroundStyle(.primary)
-				}
-			})
+			JourneySearchView.topBarLeadingToolbar(topBarAlertVM: alertVM)
 		}
 	}
 }
