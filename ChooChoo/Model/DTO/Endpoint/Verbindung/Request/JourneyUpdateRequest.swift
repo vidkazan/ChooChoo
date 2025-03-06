430df0fbcd7474d91940725b904018ad39997755
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-03-06 10:22:09 +0100

    Endpoint: added JourneyUpdate Request

---
+++ ChooChoo/Model/DTO/Endpoint/Verbindung/Request/JourneyUpdateRequest.swift
@@ -0,0 +1,11 @@
+//
+//  JourneyUpdateRequest.swift
+//  ChooChoo
+//
+//  Created by Dmitrii Grigorev on 06.03.25.
+//
+
+import Foundation
+
+protocol JourneyUpdateRequest : ChewDTO, Equatable {
+}
---
+++ ChooChoo/Model/DTO/Endpoint/Verbindung/Request/JourneyUpdateRequestImplIntBahnDe.swift
@@ -0,0 +1,40 @@
+//
+//  JourneyRequestImplIntBahnDe.swift
+//  ChooChoo
+//
+//  Created by Dmitrii Grigorev on 02.03.25.
+//
+
+import Foundation
+
+
+struct JourneyUpdateRequestIntBahnDe : ChewDTO {
+    var klasse: String
+    var reisende: [JourneyRequestIntBahnDe.Reisende]
+    var ctxRecon: String
+    var reservierungsKontingenteVorhanden: Bool
+    var nurDeutschlandTicketVerbindungen: Bool
+    var deutschlandTicketVorhanden: Bool
+}
+
+extension JourneyUpdateRequestIntBahnDe {
+    init() {
+        self.klasse = ""
+        self.ctxRecon = ""
+        self.reisende = [JourneyRequestIntBahnDe.Reisende.defaultValue]
+        self.reservierungsKontingenteVorhanden = false
+        self.nurDeutschlandTicketVerbindungen = false
+        self.deutschlandTicketVorhanden = false
+    }
+}
+
+extension JourneyUpdateRequestIntBahnDe : JourneyUpdateRequest {
+    init(
+        settings : JourneySettings,
+        journeyRef : String
+    ) {
+        self.init()
+        self.ctxRecon = journeyRef
+        self.nurDeutschlandTicketVerbindungen = settings.transportMode == .regional
+    }
+}
