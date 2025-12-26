890224c02637cef5a6b272f2e0ed92b7b27e7c96
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-12-26 14:12:47 +0100

    refactor(app): move app file

---
+++ ChooChoo/ChooChooApp.swift
@@ -0,0 +1,31 @@
+ //
+//  Chew_chew_SwiftUIApp.swift
+//  Chew-chew-SwiftUI
+//
+//  Created by Dmitrii Grigorev on 23.08.23.
+//
+
+import SwiftUI
+import CoreData
+import TipKit
+
+@main
+struct ChooChooApp: App {
+	var body: some Scene {
+        let container: AppContainer = AppContainerImpl.shared
+        let builder: NavigationViewBuilder = .init(
+            container: container,
+            router: .init()
+        )
+        
+		WindowGroup {
+            ContentView(
+                navViewBuilder: .init(
+                    container: builder.container,
+                    router: builder.router
+                )
+            )
+		}
+	}
+}
+
