dae9e4b0d00c7f1c265acbcce3474651afe47db1
Dmitrii Grigorev <vid.kazan@gmail.com>
2025-12-26 08:41:47 +0100

    chore(project): put some files back

---
+++ ChooChoo/Model/DTO/Endpoint/Repo/LocationsRepo/LocationsEndpoint.swift
@@ -0,0 +1,10 @@
+//
+//  LocationsEndpoint.swift
+//  ChooChoo
+//
+//  Created by Dmitrii Grigorev on 15.08.25.
+//
+
+import Foundation
+import CoreLocation
+
--- ChooChoo/Model/Errors/ChooError.swift
+++ ChooChoo/Model/Errors/ChooError.swift
@@ -11,31 +11,3 @@ protocol ChewError : Error, Hashable {
 //    var description : String { get }
     var localizedDescription : String { get }
 }
-
-enum DataError : ChewError {
-    static func == (lhs: DataError, rhs: DataError) -> Bool {
-        return lhs.localizedDescription == rhs.localizedDescription
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        switch self {
-        case .generic,.nilValue,.validationError:
-            break
-        }
-    }
-    case validationError(msg: String)
-    case nilValue(type : String)
-    case generic(msg: String)
-    
-    var localizedDescription : String  {
-        switch self {
-        case .validationError(let msg):
-            return NSLocalizedString("validation error: \(msg)", comment: "DataError")
-        case .nilValue(type: let type):
-            return NSLocalizedString("nil error: \(type)", comment: "DataError")
-        case .generic(let msg):
-            return NSLocalizedString("error: \(msg)", comment: "DataError")
-        }
-    }
-}
-
